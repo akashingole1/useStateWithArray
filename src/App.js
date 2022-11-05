d468b91668836442f0422a0d7e8585a8d64888e9
Akash Ingole <akash.ingole@timesinternet.in>
2022-11-06 00:09:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+let uid = 0;
 
 function App() {
+  const [todoName, setTodoName] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAdd = () => {
+    // let prev = [...todoList];
+    // prev.push({
+    //   id: ++uid,
+    //   name: todoName,
+    // });
+    // setTodoList(prev);
+    setTodoName("");
+    setTodoList([...todoList, { id: ++uid, name: todoName }]);
+  };
+
+  const handleDelete = (idx, id) => {
+    // let prev = [...todoList];
+    // prev.splice(idx, 1);
+    // setTodoList(prev);
+    setTodoList(todoList.filter((el) => el.id !== id));
+  };
+
+  console.log(">>", todoList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Use Array as State in React</h1>
+      <div className="App">
+        <h2>Todo Notes:</h2>
+        <input
+          type={"text"}
+          value={todoName}
+          onChange={(e) => setTodoName(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
+
+        <ul>
+          {todoList.map((el, i) => {
+            return (
+              <React.Fragment key={i}>
+                <li>{el.name}</li>
+                <button onClick={() => handleDelete(i, el.id)}>Delete</button>
+              </React.Fragment>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
